a52552a8d8e12a9590f7fe2011becd2b37618ffb
Pietro <pietrosavelli04@gmail.com>
2026-01-11 13:26:13 +0100

    compila ma ha problemi di deadlock

--- Esercizio buffer multipli condivisi/buffer-condivisi.c
+++ Esercizio buffer multipli condivisi/buffer-condivisi.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <pthread.h>
+#include <unistd.h>
 
 #define SIZEMAX 10
 
@@ -21,54 +22,75 @@ int produttoriFiniti;
 pthread_mutex_t lock1 = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t lock2 = PTHREAD_MUTEX_INITIALIZER;
 
-void inizializzaBuffer(Buffer b){
-	b.count = 0;
-	b.testa = 0;
-	b.coda = 0;
-	b.buffer = malloc(sizeof(int)*SIZEMAX);
+int spazioMinimo();
+
+void inizializzaBuffer(Buffer *b){
+	b->count = 0;
+	b->testa = 0;
+	b->coda = 0;
+	b->buffer = malloc(sizeof(int)*SIZEMAX);
 }
 
 
 void *produttore(void *id){
-	for(int i=0; i<5;){
-		usleep(random()%1e6);
+	for(int i=0; i<5; i++){
+		usleep(random()%100000);
+		int daInserire = 0;
 
-		while(b.count==SIZEMAX){
+		pthread_mutex_lock(&lock1);
+		while(b1.count==SIZEMAX){
 			pthread_mutex_unlock(&lock1);
 			sched_yield();
 			pthread_mutex_lock(&lock1);
 		}
-
-		pthread_mutex_lock(&lock1);
-		int daInserire = (random() % (SIZEMAX-b1.count))+1;
+		if(SIZEMAX-b1.count!=0){
+			daInserire = (random() % (SIZEMAX-b1.count))+1;
+		}
 		for(int y=0; y<daInserire; y++){
 			b1.buffer[b1.testa] = random()%100;
 			b1.testa = (b1.testa+1)%SIZEMAX;
 			b1.count++;
 		}
-		i++
-		if(i==4){
-			produttoriFiniti++;
-		}
 		pthread_mutex_unlock(&lock1);
 	}
-	return NULL
+
+	pthread_mutex_lock(&lock1);
+	produttoriFiniti++;
+	printf("PRODUTTORE %d FINITO", *(int*)id);
+	pthread_mutex_unlock(&lock1);
+
+	return NULL;
 }
 
 // se b2 pieno stampa e cancella una parte di b2 per poter inserrie nuovi elementi
 void *consumatore(void *id){
 
 	while(1){
-		usleep(random()%1e6);
+		usleep(random()%1000000);
 
 		pthread_mutex_lock(&lock2);
-		int daPulire = (random()%b2.count)+1
-		printf("PULISCO PARZIALEMENTE IL B2:");
-		for(int i=0; i<(daPulire); i++){
-			printf("%d-", b2.buffer[b2.coda]);
-			b2.coda = (b2.coda+1)%SIZEMAX;
-			b2.count--;
+
+		//se b2 vuoto e produzione finita
+		if(b2.count==0){
+			pthread_mutex_lock(&lock1);
+			if(produttoriFiniti==produttori && b1.count==0){
+				pthread_mutex_unlock(&lock1);
+				pthread_mutex_unlock(&lock2);
+				break;
+			}
+			pthread_mutex_unlock(&lock1);
 		}
+
+		if(b2.count!=0){
+			int daPulire = (random()%b2.count)+1;
+			printf("PULISCO PARZIALEMENTE IL B2:");
+			for(int i=0; i<(daPulire); i++){
+				printf("%d-", b2.buffer[b2.coda]);
+				b2.coda = (b2.coda+1)%SIZEMAX;
+				b2.count--;
+			}
+		}
+
 		pthread_mutex_unlock(&lock2);
 	}
 	return NULL;
@@ -76,18 +98,20 @@ void *consumatore(void *id){
 
 // ogni 2 sec sposto il buffer b1 dentro il buffer b2
 void *tramite(void *nome){
-	for(int i=0; i<5; i++){
-		usleep(random()%1e6);
+	while(1){
+		usleep(random()%1000000);
+		int nSposta = 0;
+
 		pthread_mutex_lock(&lock1);
 		pthread_mutex_lock(&lock2);
 
-		while(b2.count==SIZEMAX){
+		if (produttoriFiniti == produttori && b1.count == 0) {
+			pthread_mutex_unlock(&lock2);
+			pthread_mutex_unlock(&lock1);
+			break;; // ESCI DAL THREAD
+		}
 
-			if (produttoriFiniti == produttori && b1.count == 0) {
-        		pthread_mutex_unlock(&lock2);
-        		pthread_mutex_unlock(&lock1);
-        		return NULL; // ESCI DAL THREAD
-    		}
+		while(b2.count==SIZEMAX  && b1.count>0){
 			pthread_mutex_unlock(&lock2);
 			pthread_mutex_unlock(&lock1);
 			sched_yield();
@@ -95,21 +119,28 @@ void *tramite(void *nome){
 			pthread_mutex_lock(&lock2);
 		}
 
-		int nSposta = (random()%spazioMinimo())+1;
-		for(int y=0; y<nSposta; y++){
-			int appoggio = b1.buffer[b1.coda]
-			b1.coda = (b1.coda+1)%SIZEMAX;
-			b1.count--;
+		if(b1.count>0 && b2.count<SIZEMAX){
+			if(spazioMinimo()!=0){
+				nSposta = (random()%spazioMinimo())+1;
+			}
+			
+			if(nSposta>0){
+				for(int y=0; y<nSposta; y++){
+					int appoggio = b1.buffer[b1.coda];
+					b1.coda = (b1.coda+1)%SIZEMAX;
+					b1.count--;
 
-			b2.buffer[b2.testa] = appoggio;
-			b2.testa = (b2.testa+1)%SIZEMAX;
-			b2.count++;
+					b2.buffer[b2.testa] = appoggio;
+					b2.testa = (b2.testa+1)%SIZEMAX;
+					b2.count++;
+				}					
+			}
+		
 		}
-
 		pthread_mutex_unlock(&lock2);
 		pthread_mutex_unlock(&lock1);
 	}
-	return NULL
+	return NULL;
 }
 
 
@@ -134,8 +165,8 @@ int main(int argc, char const *argv[]){
 	produttori = atoi(argv[1]);
 	consumatori = atoi(argv[2]);
 	produttoriFiniti = 0;
-	inizializzaBuffer(b1);
-	inizializzaBuffer(b2);
+	inizializzaBuffer(&b1);
+	inizializzaBuffer(&b2);
 
 
 	pthread_t *prod = malloc(sizeof(pthread_t)*produttori);
@@ -145,6 +176,7 @@ int main(int argc, char const *argv[]){
 	int *idProd = malloc(sizeof(int)*produttori);
 	int *idCons = malloc(sizeof(int)*consumatori);
 
+	printf("creazione thread\n");
 	for(int i=0; i<produttori; i++){
 		idProd[i]=i+1;
 		pthread_create(&prod[i], NULL, produttore, &idProd[i]);
@@ -162,7 +194,7 @@ int main(int argc, char const *argv[]){
 	for(int i=0; i<consumatori; i++){
 		pthread_join(cons[i], NULL);
 	}
-	pthread_join(tram, NULL)
+	pthread_join(tram, NULL);
 
 
 	free(prod);
